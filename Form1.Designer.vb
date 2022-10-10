b941f7d1e9a61e2e10e462e8cf45ad5e8719221f
fanqi <fanqi@DESKTOP-EO60T56>
2022-10-10 13:34:01 +0800

    添加项目文件。

---
+++ Form1.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.ListBox_Information = New System.Windows.Forms.ListBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.FolderBrowserDialog2 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(8, 10)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(481, 70)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "输入"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(121, 40)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "选择路径"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(40, 40)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "选择文件"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 12)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "路径"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(40, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(426, 21)
+        Me.TextBox1.TabIndex = 0
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.TextBox2)
+        Me.GroupBox2.Location = New System.Drawing.Point(8, 94)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(481, 70)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "输出"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(40, 40)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "选择路径"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 18)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 12)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "路径"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(40, 13)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(426, 21)
+        Me.TextBox2.TabIndex = 2
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Controls.Add(Me.Button4)
+        Me.GroupBox3.Location = New System.Drawing.Point(314, 170)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(175, 70)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "操作"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(6, 20)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(50, 44)
+        Me.Button4.TabIndex = 0
+        Me.Button4.Text = "转换"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        Me.OpenFileDialog1.Filter = "*.bmp|*.BMP"
+        '
+        'ListBox_Information
+        '
+        Me.ListBox_Information.FormattingEnabled = True
+        Me.ListBox_Information.ItemHeight = 12
+        Me.ListBox_Information.Items.AddRange(New Object() {"信息"})
+        Me.ListBox_Information.Location = New System.Drawing.Point(8, 255)
+        Me.ListBox_Information.Name = "ListBox_Information"
+        Me.ListBox_Information.Size = New System.Drawing.Size(481, 88)
+        Me.ListBox_Information.TabIndex = 3
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Location = New System.Drawing.Point(8, 170)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(300, 70)
+        Me.GroupBox4.TabIndex = 3
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "参数"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(72, 20)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 12)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Label3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(503, 356)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.ListBox_Information)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents ListBox_Information As ListBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents FolderBrowserDialog2 As FolderBrowserDialog
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,238 @@
+﻿Public Class Form1
+#Region "委托定义"
+    'textbox：
+    Delegate Sub AddShowTextBoxDelegate(textbox As System.Windows.Forms.TextBox, str As String) '//托管程序
+    ''' <summary>
+    ''' 修改TextBox中内容
+    ''' </summary>
+    ''' <param name="textbox">TextBox控件的名称</param>
+    ''' <param name="str">需要修改的值</param>
+    ''' <remarks></remarks>
+    Private Sub DG_ChangeTextBoxValue(textbox As System.Windows.Forms.TextBox, str As String) '//托管程序
+        If (textbox.InvokeRequired) Then
+            Dim d As New AddShowTextBoxDelegate(AddressOf DG_ChangeTextBoxValue)
+            textbox.Invoke(d, textbox, str)
+        Else
+            textbox.Text = str
+        End If
+    End Sub
+
+    'Label:
+    Delegate Sub AddShowLabelDelegate(label As System.Windows.Forms.Label, str As String, Backcolor As Color) '//托管程序
+    ''' <summary>
+    ''' 修改Label的内容及背景色
+    ''' </summary>
+    ''' <param name="label">Label控件的名称</param>
+    ''' <param name="str">Label的内容</param>
+    ''' <param name="Backcolor">需要显示的背景色，默认为WhiteSmoke</param>
+    ''' <remarks></remarks>
+    Private Sub DG_ChangeLabelValue(label As System.Windows.Forms.Label, str As String, Optional Backcolor As System.Drawing.Color = Nothing) '//托管程序
+        If (label.InvokeRequired) Then
+            Dim d As New AddShowLabelDelegate(AddressOf DG_ChangeLabelValue) '//可选择为txt sub同样效果
+            label.Invoke(d, label, str, Backcolor)
+        Else
+            label.Text = str
+            If Backcolor = Nothing Then
+                Backcolor = Color.WhiteSmoke
+            End If
+            label.BackColor = Backcolor
+        End If
+    End Sub
+
+
+#End Region
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            TextBox1.Text = OpenFileDialog1.FileName
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If FolderBrowserDialog2.ShowDialog() = DialogResult.OK Then
+            TextBox2.Text = FolderBrowserDialog2.SelectedPath
+        End If
+    End Sub
+
+    Public myFloderOperator As FloderOperator = New FloderOperator
+    Public finishnumtext As String
+    Public allbmpnum, finishnum As Integer
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If isworking = False Then
+            Dim mythread1 As Threading.Thread
+            mythread1 = New Threading.Thread(AddressOf ConvertThread)
+            mythread1.Start()
+        Else
+            MessageBox.Show("请等待运行完成")
+        End If
+    End Sub
+    Public isworking As Boolean = False
+    Private Sub ConvertThread()
+        isworking = True
+        bmpfilelist = New List(Of BMPPathAndName)
+        bmpfilelist.Clear()
+        finishnum = 0
+        Try
+            If IO.File.Exists(TextBox1.Text) Then
+                myFloderOperator.CreatFloder(TextBox2.Text)
+                ConvertBMP2JPG(TextBox1.Text, TextBox2.Text + "\" + myFloderOperator.GetFileName(TextBox1.Text).Replace("bmp", "jpg"))
+                AddInfo(String.Format("转换{0}成功", TextBox1.Text))
+            ElseIf IO.Directory.Exists(TextBox1.Text) Then
+                GetBMPList(TextBox1.Text, TextBox1.Text)
+                For Each bmpfile In bmpfilelist
+                    ConvertBMP2JPG(bmpfile.pathAbs, TextBox2.Text + "\" + bmpfile.PathRelative.Replace("bmp", "jpg"))
+                    finishnum += 1
+                    DG_ChangeLabelValue(Label3, String.Format("{0}/{1}", finishnum.ToString, allbmpnum.ToString))
+                    AddInfo(String.Format("转换{0}成功", bmpfile.pathAbs))
+                Next
+            Else
+                MessageBox.Show("BMP文件输入路径不合法")
+            End If
+        Catch ex As Exception
+            AddInfo("转换过程中发生错误")
+            AddInfo(ex.ToString())
+            MessageBox.Show("转换过程中发生错误：" + ex.ToString())
+        End Try
+        isworking = False
+    End Sub
+
+    Public bmpfilelist As List(Of BMPPathAndName)
+    Public Structure BMPPathAndName
+        Dim pathAbs As String
+        Dim PathRelative As String
+        Dim name As String
+    End Structure
+    Public Sub GetBMPList(path As String, orgPath As String)
+        Dim mDirinfo As System.IO.DirectoryInfo = New IO.DirectoryInfo(path)
+        Dim mDirs() As IO.DirectoryInfo = mDirinfo.GetDirectories
+        Dim mFiles() As IO.FileInfo = mDirinfo.GetFiles("*.bmp")
+        Dim mFiles2() As IO.FileInfo = mDirinfo.GetFiles("*.BMP")
+        For Each mFile In mFiles
+            Dim PN As BMPPathAndName
+            PN.pathAbs = mFile.FullName
+            PN.PathRelative = PN.pathAbs.Substring(orgPath.Length, PN.pathAbs.Length - orgPath.Length)
+            PN.name = mFile.Name
+            bmpfilelist.Add(PN)
+        Next
+        For Each mFile In mFiles2
+            Dim PN As BMPPathAndName
+            PN.pathAbs = mFile.FullName
+            PN.PathRelative = PN.pathAbs.Substring(orgPath.Length, PN.pathAbs.Length - orgPath.Length)
+            PN.name = mFile.Name
+            bmpfilelist.Add(PN)
+        Next
+        For Each mDir In mDirs
+            GetBMPList(mDir.FullName, orgPath)
+        Next
+        allbmpnum = bmpfilelist.Count
+    End Sub
+
+    Public Sub ConvertBMP2JPG(BMPpath As String, JPGpath As String)
+        Try
+            If IO.File.Exists(BMPpath) = True Then
+                Dim image As Bitmap = New Bitmap(BMPpath)
+                myFloderOperator.CreatFloder(JPGpath)
+                image.Save(JPGpath, Imaging.ImageFormat.Jpeg)
+            Else
+
+            End If
+        Catch ex As Exception
+            AddInfo(ex.ToString())
+        End Try
+    End Sub
+
+
+#Region "其他维护性工作"
+    Delegate Sub AddToInfoList(ByVal txt As String)
+    Public ApplicationPath As String = My.Application.Info.DirectoryPath
+    Public LogPath As String
+    ''' <summary>
+    ''' 在界面列表中添加信息
+    ''' </summary>
+    ''' <param name="str">需要添加的信息</param>
+    ''' <remarks></remarks>
+    ''' 
+    Public Sub AddInfo(ByVal str As String)
+        Dim messageinfo As New AddToInfoList(AddressOf AddInfoMain)
+        Me.Invoke(messageinfo, str)
+    End Sub
+    Private Sub AddInfoMain(ByVal str As String)
+        str = Now.TimeOfDay.ToString() + " ：" + str
+        ListBox_Information.Items.Add(str)
+        ListBox_Information.SelectedItem = str
+        If ListBox_Information.Items.Count > 3000 Then ListBox_Information.Items.Clear()
+        LogPath = ApplicationPath + "\Log\" + Now.Year.ToString() + "-" + Now.Month.ToString() + "-" + Now.Day.ToString() + ".Log"
+        If Not IO.File.Exists(LogPath) Then
+            'IO.File.Create(LogPath)
+            Dim fs As System.IO.FileStream = IO.File.Create(LogPath)
+            fs.Close()
+        End If
+        Dim sw As New System.IO.StreamWriter(LogPath, True)
+        sw.WriteLine(str)
+        sw.Flush()
+        sw.Close()
+    End Sub
+    Private Sub AddInfoToLog(ByVal str As String)
+        Dim sw As New System.IO.StreamWriter(LogPath, True)
+        sw.WriteLine(str)
+        sw.Flush()
+        sw.Close()
+    End Sub
+#End Region
+
+End Class
+Public Class FloderOperator
+    ''' <summary>
+    ''' 判断此路径是否是文件
+    ''' 主要通过最后的文件名中是否存在"."
+    ''' </summary>
+    ''' <param name="path"></param>
+    ''' <returns></returns>
+    Public Function IsFilePath(path As String) As Integer
+        Dim mDirNames() As String = path.Split("\")
+        If InStr(mDirNames(mDirNames.Length - 1), ".") > 0 Then
+            Return mDirNames(mDirNames.Length - 1).Length
+        End If
+        Return 0
+    End Function
+    ''' <summary>
+    ''' 如果输入的是文件路径，则移除文件，保留文件夹的路径。如果是文件夹路径，则原封不动返回
+    ''' </summary>
+    ''' <param name="path"></param>
+    ''' <returns></returns>
+    Public Function RemoveFilePath(path As String) As String
+        If IsFilePath(path) = 0 Then
+            Return path
+        Else
+            Return path.Substring(0, path.Length - IsFilePath(path))
+        End If
+    End Function
+    ''' <summary>
+    ''' 返回路径中的文件名
+    ''' </summary>
+    ''' <param name="path"></param>
+    ''' <returns></returns>
+    Public Function GetFileName(path As String) As String
+        Return path.Substring(path.Length - IsFilePath(path), IsFilePath(path))
+    End Function
+    ''' <summary>
+    ''' 创建文件夹
+    ''' </summary>
+    ''' <param name="path"></param>
+    Public Sub CreatFloder(path As String)
+        Dim dirpath As String = RemoveFilePath(path)
+        If IO.Directory.Exists(IO.Directory.GetParent(dirpath).FullName) = True Then
+            IO.Directory.CreateDirectory(dirpath)
+        Else
+            CreatFloder(IO.Directory.GetParent(dirpath).FullName)
+            IO.Directory.CreateDirectory(dirpath)
+        End If
+    End Sub
+End Class
